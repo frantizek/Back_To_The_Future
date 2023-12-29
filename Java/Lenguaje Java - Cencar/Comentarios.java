ea68987611d8637955322eaca862d19779ef5fcd
José Francisco Ruvalcaba Castañeda <32653773+frantizek@users.noreply.github.com>
2023-12-29 04:51:27 +0000

    primeros ejemplos del manual

---
+++ Java/Lenguaje Java - Cencar/Comentarios.java
@@ -0,0 +1,14 @@
+/**
+ * Estoso son comentarios que seran desplegados por el javadoc,
+ * para la clase HolaMundo que hereda de la clase Object.
+ * @see java.lang.Object
+ */
+public class Comentarios {
+    /**
+     * este metodo es el principal de la clase y toma un parametro
+     * @param args[] almacena los argumentos de la linea de comandos 
+     */
+    public static void main(String args[]){
+        System.out.println("¡Hola mundo!");
+    }    
+}
---
+++ Java/Lenguaje Java - Cencar/Modulus.java
@@ -0,0 +1,12 @@
+public class Modulus {
+
+    // El operador modulo de java tambien funciona con numeros de punto
+    // flotante como muestra el siguiente ejemplo:
+
+    public static void main(String[] args) {
+        int x = 42;
+        double y = 42.3;
+        System.out.println("x mod 10 = " + x % 10);
+        System.out.println("y mod 10 = " + y % 10);
+    }
+}
